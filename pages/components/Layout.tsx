5f4aca545c20c9adf8374262e2b1ca87d687e716
uta0463 <xxxuta0463xxx@gmail.com>
2022-09-01 15:24:16 +0900

    update ga4

--- pages/components/Layout.tsx
+++ pages/components/Layout.tsx
@@ -13,6 +13,18 @@ const Layout = ({ children }: Props) => {
       <Head>
         <meta property="og:locale" content="ja_JP" />
         <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico"></link>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-CD8T6Y0P1B"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+    
+              gtag('config', 'G-CD8T6Y0P1B');
+            `,
+          }}
+        />
       </Head>
 
       <Header></Header>
