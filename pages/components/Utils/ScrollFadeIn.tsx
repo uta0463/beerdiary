9c45459dae4958bcd8bfadbc47f4e5f6a7d0664d
uta0463 <xxxuta0463xxx@gmail.com>
2022-09-16 15:31:30 +0900

    update scroll animations

--- pages/components/Utils/ScrollFadeIn.tsx
+++ pages/components/Utils/ScrollFadeIn.tsx
@@ -1,18 +1,26 @@
 const ScrollFadeIn = () => {
-  const callback = (entries: any) => {
-    entries.forEach((entry: any) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('is-slideIn');
-      }
-    })
+  if (typeof window !== "undefined") {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.2
+    };
+
+    const callback = (entries: any) => {
+      entries.forEach((entry: any) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('is-slideIn');
+        }
+      })
+    }
+
+    const observer = new IntersectionObserver(callback, options);
+
+    const targets = document.querySelectorAll('.u__animation__fadeIn');
+    targets.forEach((target) => {
+      observer.observe(target);
+    });
   }
-
-  const observer = new IntersectionObserver(callback, { threshold: 0.2 });
-
-  const targets = document.querySelectorAll('.u__animation__fadeIn');
-  targets.forEach((target) => {
-    observer.observe(target);
-  });
 }
 
 export default ScrollFadeIn;
